e235db5b6eae25fa9918c481b1b3a884a315f143
Alex <aleksulsk@yandex.ru>
2024-08-27 14:38:48 +0400

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -23,13 +23,10 @@ class House:
         isinstance(other, int)
         return self.number_of_floors < other.number_of_floors
     def __le__(self, other):
-        #isinstance(other, int)
         return self.number_of_floors <= other.number_of_floors
     def __gt__(self, other):
-        #isinstance(other, int)
         return self.number_of_floors > other.number_of_floors
     def __ge__(self, other):
-        #isinstance(other, int)
         return self.number_of_floors >= other.number_of_floors
     def __ne__(self, other):
         return self.number_of_floors != other.number_of_floors
